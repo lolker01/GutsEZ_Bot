bdf89b7d19103044f6ed2cfb464c825269ebfbc3
Svyatoslav Yaremchuk <lolker01.top@gmail.com>
2024-03-28 15:46:32 +0200

    dded some new fitures

--- main.py
+++ main.py
@@ -16,13 +16,33 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello, i am Guts_bot! How can i help you?')
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    message = update.message.text.lower()
-    if 'привіт' or 'hello' or 'hi' in message:
-        await update.message.reply_text(f'Привіт {update.effective_user.first_name}')
-    else:
-        await update.message.reply_text(f'Шо ти висрав? {update.effective_user.first_name}')
+    last_name = update.effective_user.last_name
 
-    await update.message.reply_text(reply_text)
+    message = update.message.text.lower()
+
+    if "привіт" in message:
+
+        if last_name is None:
+            await update.message.reply_text(f'Привіт {update.effective_user.first_name}')
+
+        else:
+            await update.message.reply_text(f'Привіт {update.effective_user.first_name} {update.effective_user.last_name}')
+
+    elif "допобачення" in message:
+
+        if last_name is None:
+            await update.message.reply_text(f'Бувай {update.effective_user.first_name}')
+
+        else:
+            await update.message.reply_text(f'Привіт {update.effective_user.first_name} {update.effective_user.last_name}')
+
+    else:
+
+        if last_name is None:
+            await update.message.reply_text(f'{update.effective_user.first_name}, Шо ти висрав?')
+
+        else:
+            await update.message.reply_text(f'{update.effective_user.first_name} {update.effective_user.last_name}, Шо ти висрав?')
 
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
